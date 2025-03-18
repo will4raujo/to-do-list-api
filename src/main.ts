0ad79d3b4f39620e18d8062f0c8bf36311eeb5ff
willian araujo <willian.araujo1994@gmail.com>
2025-03-18 19:50:54 -0300

    feat: enableCors & prettier setup semi as true

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`🚀 Backend is running on port ${process.env.PORT ?? 3000}`);
+  });
 }
-bootstrap();
+void bootstrap();
