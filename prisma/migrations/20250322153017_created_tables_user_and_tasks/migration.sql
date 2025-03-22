d92f623ba343191bec9d4b7575939521ad4dc7dd
willian araujo <willian.araujo1994@gmail.com>
2025-03-22 12:41:10 -0300

    feat: prisma setup
    - Installed Prisma in the project dependencies
    - Created Users and Tasks tables
    - Created Status enum TaskStatus

---
+++ prisma/migrations/20250322153017_created_tables_user_and_tasks/migration.sql
@@ -0,0 +1,29 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('OPEN', 'IN_PROGRESS', 'DONE');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "status" "TaskStatus" NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
